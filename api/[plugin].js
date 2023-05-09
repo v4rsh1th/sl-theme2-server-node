326866cc39eca12c0ec07bd5af44f6c4cbfe5f1d
Thota <vt8fn@umsystem.edu>
2023-05-09 08:19:18 -0500

    init

---
+++ api/[plugin].js
@@ -0,0 +1,24 @@
+const { readdirSync } = require('fs');
+const { join } = require('path');
+
+export default (req, res) => {
+  const {
+    query: { plugin },
+  } = req;
+
+  try {
+    const zips = readdirSync(join(process.cwd(), 'plugins', plugin));
+
+    // Passing the latest file, the previous file can be used as an older version
+    const latestVersion = zips[1];
+
+    return res.json({
+      version: latestVersion,
+      package: `http://localhost:3000/plugins/${plugin}/${latestVersion}`
+    })
+  } catch (e) {
+    return res.status(404).json({
+      message: 'Plugin not found',
+    });
+  };
+};
